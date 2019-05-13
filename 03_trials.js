f3a1cc512628504d7842042b08b64b2823e0274d
carolinalena <47942241+carolinalena@users.noreply.github.com>
2019-05-13 17:19:19 +0200

    Update 03_trials.js

--- 03_trials.js
+++ 03_trials.js
@@ -4,41 +4,46 @@
 const trial_info = {
     keyPress: [
         {
-            key1: 'd',
+            question: "Are the objects the same 'd' or different 'k'?"
+	    key1: 'd',
 	    key2: 'k',
-            picture: 'images/1_150_same.jpg',
+            picture: "images/1_150_same.jpg",
             d: 'same',
             k: 'different',
 	    expected: 'same'
         },
 	{
+	    question: "Are the objects the same 'd' or different 'k'?"
             key1: 'd',
 	    key2: 'k',
-            picture: 'images/1_50_different.jpg',
+            picture: "images/1_50_different.jpg",
             d: 'same',
             k: 'different',
 	    expected: 'different'
         },
 	{
+	    question: "Are the objects the same 'd' or different 'k'?"
             key1: 'd',
 	    key2: 'k',
-            picture: 'images/3_150_same.jpg',
+            picture: "images/3_150_same.jpg",
             d: 'same',
             k: 'different',
 	    expected: 'same'
         },
 	{
+	    question: "Are the objects the same 'd' or different 'k'?"
             key1: 'd',
 	    key2: 'k',
-            picture: 'images/6_150_different.jpg',
+            picture: "images/6_150_different.jpg",
             d: 'same',
             k: 'different',
 	    expected: 'different'
         },
 	{
+	    question: "Are the objects the same 'd' or different 'k'?"
             key1: 'd',
 	    key2: 'k',
-            picture: 'images/7_50_same.jpg',
+            picture: "images/7_50_same.jpg",
             d: 'same',
             k: 'different',
 	    expected: 'same'
